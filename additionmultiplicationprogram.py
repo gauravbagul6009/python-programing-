8345e9362b6c6a095ecc651e1e9c6513936adb29
unknown <gauravbagul0709@gmail.com>
2023-08-27 19:07:46 +0530

    adding python file 26aug23t

---
+++ additionmultiplicationprogram.py
@@ -0,0 +1,22 @@
+var1=int(input("enter your first"))
+var2=int(input("enter second number"))
+var3=var1-var2
+var3=var1/var2
+print("substractionn is:"+str(var3))
+print("dividation is:"+str(var3))
+var1=float(input("enter first number"))
+var2=float(input("enter second number"))
+var3=var1+var2
+var4=var1*var2
+print("addition is:"+str(var3))
+print("multiplicatin is:"+str(var4))
+var1=int(input("give yor number"))
+var2=float(input("give your second number"))
+var3=var1+var2
+print("multiplicatin is:"+str(var3))
+print("addition id:"+str(var3))10
+
+
+
+
+
---
+++ subtractiondividationprogram.py/comparition.py
@@ -0,0 +1,37 @@
+a=float(input("give any no of your choice"))
+
+if (a<10):
+    print ("thena a is less than 10")
+elif (a>10 and a<=20):
+    print("then a is greter thann 10 and less than or equal to 20")
+elif (a>20 and a<=30):
+    print("then a is greter thann 20 and less than or equal to 30") 
+elif (a>30 and a<=40):
+    print("then a is greter thann 30 and less than or equal to 40")
+elif (a>40 and a<=50):
+    print("then a is greter thann 40 and less than or equal to 50")
+elif (a>50 and a<=60):
+    print("then a is greter thann 50 and less than or equal to 60")
+elif (a>60 and a<=70):
+    print("then a is greter thann 60 and less than or equal to 70")
+elif  (a>70 and a<=80):
+    print("than a lies betweeen 70 to 80")
+elif (a>80 and a<=90):
+    print ("a is lies between 80 to 90")
+elif (a>90 and a<=100):
+    print ("a islies between 90 to 100")  
+else: 
+   print ("a is greater thann 100")
+
+nam=(input("enter your name first name alphabet"))
+
+if (nam k):
+    print("you are luky"+str("nam"))
+else:
+    print ("you are bad gye")
+    
+
+
+
+
+
